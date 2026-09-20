3e085f6e5d36e322c7e23c82ed2398bc7d3c5f38
ibadgtm-hash <ibadgtm@gmail.com>
2026-09-20 08:39:11 +0700

    Update build.gradle.kts
    File

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,9 +11,17 @@ android {
         applicationId = "com.example.arrowbot"
         minSdk = 26
         targetSdk = 35
-
         versionCode = 1
         versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
